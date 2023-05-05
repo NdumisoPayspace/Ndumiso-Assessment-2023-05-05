ab3e15ffdde27d1236710f5b719927632cde781d
ndumisoz <ndumisoz@NDUMISOZ-SSD>
2023-05-05 16:56:11 +0200

    Added Add new person functionality.

--- APIControllers/PeopleAPIController.cs
+++ APIControllers/PeopleAPIController.cs
@@ -6,15 +6,27 @@
     using Microsoft.AspNetCore.Mvc;
 
     using Ndumiso_Assessment_2023_05_05.Models;
+    using Newtonsoft.Json;
 
     [Route("api/[controller]")]
-    [ApiController]
-    public class PeopleAPIController : ControllerBase
+    public class PeopleAPIController : Controller
     {
         [HttpGet]
         public object Get(DataSourceLoadOptions loadOptions)
         {
             return DataSourceLoader.Load(MockData.people.ToList(), loadOptions);
+        }
+
+        [HttpPost]
+        public IActionResult Post(string values)
+        {
+            var newPerson = new Person();
+
+            JsonConvert.PopulateObject(values, newPerson);
+
+            MockData.people.Add(newPerson);
+
+            return this.Ok(newPerson);
         }
     }
 }
